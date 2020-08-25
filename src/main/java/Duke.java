326912ff6fda96c6043598ad7badf364096959ed
dozenmatter <chinheng.97@gmail.com>
2020-08-25 20:09:26 +0800

    Add Increment "Level-3: Mark as Done"

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,38 +4,59 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        introduce();
+
+        Scanner sc = new Scanner(System.in);
+        List<Task> tasks = new ArrayList<Task>();
+        while (sc.hasNextLine()) {
+            String input = sc.nextLine().trim();
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                return;
+            }
+
+            if (input.equals("list")){
+                if (tasks.isEmpty()){
+                    System.out.println("The list is currently empty!");
+                } else {
+                    System.out.println("Here are the tasks in your list:");
+                    for (int i = 0; i < tasks.size(); i++){
+                        Task t = tasks.get(i);
+                        System.out.printf("%d.[%s] %s\n", (i+1), t.getStatusIcon(), t.getDescription());
+                    }
+                }
+                continue;
+            }
+
+            if (input.matches("done [0-9]+")) {
+                int id = Integer.parseInt(input.split(" ")[1]) - 1;
+
+                Task task = tasks.get(id);
+
+                if (task.isDone) {
+                    System.out.println("Task has been completed!");
+                    continue;
+                }
+
+                task.markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.printf(" [%s] %s\n", task.getStatusIcon(), task.getDescription());
+                continue;
+            }
+
+            tasks.add(new Task(input));
+            System.out.println("added: " + input);
+        }
+    }
+
+    public static void introduce(){
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-
-        System.out.println("____________________________________________________________\n" +
-                " Hello! I'm Duke\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________");
-        List<String> al = new ArrayList<String>();
-        while(true){
-            Scanner in = new Scanner(System.in);
-            String input = in.nextLine();
-            if (input.equals("bye")) {
-                System.out.println(" Bye. Hope to see you again soon!");
-                return;
-            }
-
-            if (input.equals("list")){
-                if (al.isEmpty()){
-                    System.out.println("The list is currently empty!");
-                } else {
-                    for (int i = 0; i < al.size(); i++){
-                        System.out.println((i+1) + ". " + al.get(i));
-                    }
-                }
-                continue;
-            }
-            al.add(input);
-            System.out.println("added: " + input);
-        }
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
     }
 }
